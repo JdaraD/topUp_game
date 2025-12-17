367265fd70b455256924ae1d08b1d12e514e20ba
JdaraD <juliandara17@gmail.com>
2025-12-17 15:13:54 +0700

    update footer

--- resources/views/livewire/layouts/footer.blade.php
+++ resources/views/livewire/layouts/footer.blade.php
@@ -1,40 +1,62 @@
-<div class="flex flex-col py-4 items-center gap-4 bg-[#212121] w-full h-125">
-    <div class="flex w-[80%] gap-8">
+<div class="flex flex-col py-12 items-center justify-center bottom-0 gap-12 bg-[#212121] w-full h-auto">
+    <div class="flex flex-col w-[80%]">
         <div>
-            <p class="text-white">test</p>
-            
+            <img src="{{ asset('image/jd.png') }}" alt="" class="h-14 py-0.5 w-14">
+        
         </div>
-        <div>
-            <p class="text-[#8e7a5c] capitalize">peta situs</p>
-            <ul class="capitalize">
-                <li>beranda</li>
-                <li>cek transaksi</li>
-                <li>hubungi kami</li>
-                <li>ulasan</li>
-            </ul>
+        <div class="flex gap-8">
+            <div class="flex flex-col w-[40%] gap-4">
+                <p class="text-white text-justify">OURA STORE adalah tempat top up games yang aman, murah dan terpercaya. Proses cepat 1-3 Detik. Open 24 jam. Payment terlengkap. Jika ada kendala silahkan klik logo CS pada kanan bawah di website ini.</p>
+                <div class="flex gap-2">
+                    <a href="#">
+                        <img src="{{ asset('image/ig.png') }}" alt="" class="h-10 w-10">
+                    </a>
+                    <a href="#">
+                        <img src="{{ asset('image/tiktok.png') }}" alt="" class="h-10 w-10">
+                    </a>
+                    <a href="#">
+                        <img src="{{ asset('image/gmail.png') }}" alt="" class="h-10 w-10">
+                    </a>
+                    <a href="#">
+                        <img src="{{ asset('image/youtube.png') }}" alt="" class="h-10 w-10">
+                    </a>
+       
+                </div>
+                
+            </div>
+            <div class="flex flex-col w-[10%] gap-4">
+                <h3 class="text-[#8e7a5c] capitalize">peta situs</h3>
+                <ul class="flex flex-col gap-1 capitalize text-white">
+                    <li>beranda</li>
+                    <li>cek transaksi</li>
+                    <li>hubungi kami</li>
+                    <li>ulasan</li>
+                </ul>
+            </div>
+            <div class="flex flex-col w-[10%] gap-4">
+                <h3 class="text-[#8e7a5c] capitalize">dukungan</h3>
+                <ul class="flex flex-col gap-1 capitalize text-white">
+                    <li>whatsapp</li>
+                    <li>instagram</li>
+                    <li>email</li>
+                </ul>
+            </div>
+            <div class="flex flex-col w-[10%] gap-4">
+                <h3 class="text-[#8e7a5c] capitalize">legalitas</h3>
+                <ul class="flex flex-col gap-1 capitalize text-white">
+                    <li>kebijakan pribadi</li>
+                    <li>syarat & ketentuan</li>
+                </ul>
+            </div>
         </div>
-        <div>
-            <p class="text-[#8e7a5c] capitalize">dukungan</p>
-            <ul class="capitalize">
-                <li>whatsapp</li>
-                <li>instagram</li>
-                <li>email</li>
-            </ul>
-        </div>
-        <div>
-            <p class="text-[#8e7a5c] capitalize">legalitas</p>
-            <ul class="capitalize">
-                <li>kebijakan pribadi</li>
-                <li>syarat & ketentuan</li>
-            </ul>
-        </div>
+
     </div>
     <div class="border-b border-white/10 w-full"></div>
-    <div class="flex justify-between w-[80%]">
+    <div class="flex items-center justify-between w-[80%]">
         <div class="text-white select-none">
             <p>&copy; 2025 JGaming.All rights reserved</p>
         </div>
-        <button class="border rounded-md border-white/20 py-1 px-1 cursor-pointer">
+        <button class="border rounded-md border-[#8e7a5c]/10 py-1.5 px-1.5 cursor-pointer hover:bg-amber-50/10 transition">
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" style="color: white;" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-moon">
                 <path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z" />
             </svg>
