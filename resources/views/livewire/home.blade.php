b5915db48fe60b5d60cd84f078d291fc03ba7678
JdaraD <juliandara17@gmail.com>
2025-12-18 14:23:11 +0700

    update home views structure

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -1,3 +1,15 @@
 <div class="flex flex-col grow h-full bg-amber-300 border-2 border-amber-50">
-    <p class="h-full">test2</p>
+    <div class="">
+        <p class="h-full">container gambar</p>
+    </div>
+    <div class="">
+        <p class="h-full">populer game</p>
+    </div>
+    <div class="">
+        <p class="h-full">container game</p>
+    </div>
+    <div class="">
+        <p class="h-full">container slider iklan</p>
+    </div>
+
 </div>
