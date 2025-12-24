596fb1f8b044668c80d993da412f0fce4c24b27f
JdaraD <juliandara17@gmail.com>
2025-12-24 18:10:12 +0700

    update page topup

--- resources/views/livewire/top-up.blade.php
+++ resources/views/livewire/top-up.blade.php
@@ -3,8 +3,17 @@
         <div class="flex justify-center items-center bg-gray-200/20 h-91 w-full object-cover">
             <img src="{{ asset('image/bg.gif') }}" alt="" class="w-full h-full object-cover">
         </div>
-        <div class="flex bg-amber-200 h-[160.8px] w-full">
-    
+        <div class="flex relative bg-linear-to-r from-[#1c1f22] to-[#2a2f34] justify-center items-center h-[160.8px] w-full">
+            <div class="flex relative gap-4 h-[160.8px] w-[80%]">
+                <div class="relative h-full w-[211.92px] perspective-[1000px]">
+                    <div class="absolute bottom-10 h-52 w-48 transform-3d transform-[rotateY(25deg)_rotateX(0deg)_translateZ(15px)] rounded-xl overflow-hidden shadow-[0_30px_60px_-15px_rgba(0,0,0,0.8)]">
+                        <img src="{{ asset('image/cover.png') }}" class="w-full h-full object-cover">
+                    </div>
+                </div>
+
+                <div class="flex relative gap-1 bg-yellow-400 h-full w-40"></div>
+            </div>
+            
         </div>
     </div>
 </div>
