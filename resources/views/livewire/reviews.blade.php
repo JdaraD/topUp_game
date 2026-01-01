94e112aad4a56495799f5cbc21374a64d7d5605c
JdaraD <juliandara17@gmail.com>
2026-01-01 17:33:20 +0700

    page review done

--- resources/views/livewire/reviews.blade.php
+++ resources/views/livewire/reviews.blade.php
@@ -1,13 +1,16 @@
 <div class="flex flex-col grow gap-4 justify-center items-center w-full h-full mt-27.5 select-none">
     <div class="flex flex-col gap-4 justify-center items-center h-auto w-[80%]">
-        <div class="flex flex-col gap-4 justify-center items-center">
-            <p>Ulasan</p>
-            <h2>Terima kasih kepada semua pelanggan yang memberikan ulasan dan penilaian kepada kami.</h2>
+        <div class="flex flex-col gap-4 justify-center w-full h-full items-center my-24">
+            <div class="flex flex-col gap-2 justify-center items-center w-[50%] h-auto text-center">
+                <p class="text-[#b09873] text-lg font-bold">Ulasan</p>
+                <h2 class="text-white font-bold text-3xl">Terima kasih kepada semua pelanggan yang memberikan ulasan dan penilaian kepada kami.</h2>
+
+            </div>
 
         </div>
 
-        <div class="grid grid-cols-3 h-auto items-center place-items-center gap-6 w-full">
-            {{-- @for ($i = 0; $i < 12; $i++) --}}
+        <div class="grid grid-cols-3 h-auto justify-center items-center place-items-center gap-6 w-full">
+            @for ($i = 0; $i < 7; $i++)
                 <div class="flex w-98.75 justify-center items-center h-42 bg-[#212121] rounded-md shadow-lg">
                     <div class="flex flex-col h-full justify-center w-full px-6 py-4 gap-2">
                         <p class="text-white text-sm font-bold">PUBG Mobile</p>
@@ -31,7 +34,7 @@
                     </div>
                 </div>
                 
-            {{-- @endfor --}}
+            @endfor
             <div class="col-span-full flex w-full h-full justify-center">
                 <button wire:click="" id="btnMore" class="border border-white/20 bg-[#262727] py-2 px-2 rounded-md hover:bg-amber-50/10 transition delay-75 cursor-pointer">
                     <p class="text-white text-xs">Tampilkan Lainya....</p>
