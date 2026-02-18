37dc87d6ab5cc9ea21672894f97fb345802e74d8
JdaraD <juliandara17@gmail.com>
2026-02-18 14:20:43 +0700

    update responsive trems and condition page

--- resources/views/livewire/trems-and-condition.blade.php
+++ resources/views/livewire/trems-and-condition.blade.php
@@ -1,19 +1,19 @@
-<div class="flex flex-col grow gap-4 justify-center items-center w-full h-full mt-27.5 select-none">
-    <div class="flex flex-col w-[60%] my-8 h-full gap-4">
-        <h1 class="text-[#8e7a5c] text-1xl font-semibold">Kebijakan Privasi</h1>
+<div class="flex flex-col grow gap-4 justify-center items-center w-full h-full lg:mt-27.5 md:mt-27.5 mt-20 select-none">
+    <div class="flex flex-col lg:w-[60%] md:w-[70%] w-[90%] lg:my-8 md:my-4 my-2 h-full lg:gap-4 md:gap-2 gap-1">
+        <h1 class="text-[#8e7a5c] lg:text-xl md:text-xl text-lg font-semibold">Kebijakan Privasi</h1>
         <p class="text-white text-justify">
             Kami di J Gaming STORE menghargai privasi Anda dan berkomitmen untuk melindungi informasi pribadi yang Anda berikan kepada kami. Kebijakan Privasi ini menjelaskan bagaimana kami mengumpulkan, menggunakan, dan melindungi informasi Anda saat Anda menggunakan layanan kami.
         </p>
-        <h2 class="text-[#8e7a5c] text-1xl font-semibold mt-4">Informasi yang Kami Kumpulkan</h2>
+        <h2 class="text-[#8e7a5c] lg:text-xl md:text-xl text-lg font-semibold mt-4">Informasi yang Kami Kumpulkan</h2>
         <p class="text-white text-justify">
             Kami dapat mengumpulkan informasi pribadi seperti nama, alamat email, nomor telepon, dan informasi pembayaran saat Anda melakukan pembelian atau berinteraksi dengan layanan kami. Selain itu, kami juga dapat mengumpulkan informasi non-pribadi seperti data penggunaan situs web dan preferensi pengguna.
         </p>
-        <h2 class="text-[#8e7a5c] text-1xl font-semibold mt-4">Penggunaan Informasi</h2>
-        <p class="text-white text-justify">
+        <h2 class="text-[#8e7a5c] lg:text-xl md:text-xl text-lg font-semibold mt-4">Penggunaan Informasi</h2>
+        <p class="text-white text-base text-justify">
             Informasi yang kami kumpulkan digunakan untuk memproses pesanan Anda, memberikan layanan pelanggan, mengirimkan pembaruan produk, dan meningkatkan pengalaman pengguna di situs web kami. Kami juga dapat menggunakan informasi Anda untuk tujuan pemasaran, tetapi hanya dengan izin Anda.
         </p>
     </div>
-    <div class="flex w-full h-70">
+    <div class="flex w-full lg:h-70 md:h-52 h-16">
         @if (file_exists(public_path('image/bgS.png')))
             <img src="{{ asset('image/bgS.png') }}" alt="" class="w-full h-full object-cover">  
             
