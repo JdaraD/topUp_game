f57f34dcb9f593a0fd2cc52484dfdf79364195df
JdaraD <juliandara17@gmail.com>
2026-02-23 14:19:46 +0700

    page login done responsive

--- app/Livewire/Auth/LoginFrom.php
+++ app/Livewire/Auth/LoginFrom.php
@@ -2,10 +2,25 @@
 
 namespace App\Livewire\Auth;
 
+use App\Models\profil;
 use Livewire\Component;
 
 class LoginFrom extends Component
 {
+    public $profil;
+
+    public function loadProfil() 
+    {
+        $this->profil = profil::where('is_active', 1)
+            ->latest()
+            ->first();
+    }
+
+    function mount()
+    {
+        $this->loadProfil();
+    }
+
     public function render()
     {
         return view('livewire.auth.login-from');
