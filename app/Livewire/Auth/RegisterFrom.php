d95361a7d6fc5364055004645f7a0e119cfc965e
JdaraD <juliandara17@gmail.com>
2026-02-24 21:12:58 +0700

    responsive device mobile page register done

--- app/Livewire/Auth/RegisterFrom.php
+++ app/Livewire/Auth/RegisterFrom.php
@@ -2,10 +2,24 @@
 
 namespace App\Livewire\Auth;
 
+use App\Models\profil;
 use Livewire\Component;
 
 class RegisterFrom extends Component
 {
+    public $profil;
+
+    public function loadProfil() 
+    {
+        $this->profil = profil::where('is_active', 1)
+            ->latest()
+            ->first();
+    }
+
+    function mount()
+    {
+        $this->loadProfil();
+    }
     public function render()
     {
         return view('livewire.auth.register-from');
--- resources/views/livewire/auth/register-from.blade.php
+++ resources/views/livewire/auth/register-from.blade.php
@@ -1,11 +1,16 @@
 <div class="flex w-full h-screen">
-    <div class="flex flex-none relative justify-center items-center bg-[#262727] w-137.5 h-full">
+    <div class="flex flex-none relative justify-center items-center bg-[#262727] lg:w-137.5 md:w-137.5 w-full h-full">
         <button onclick="window.location.href='{{ route('home') }}'" class="absolute flex inset-4 w-9 h-9 top-4 left-4 justify-center items-center bg-[#212121] cursor-pointer rounded-md hover:bg-gray-800 transition">
             <svg xmlns="http://www.w3.org/2000/svg"  width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon text-white icon-tabler icons-tabler-outline icon-tabler-x">
                 <path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M18 6l-12 12" /><path d="M6 6l12 12" />
             </svg>
         </button>
-        <div class="flex flex-col w-[80%] h-fit gap-4">
+        <div class="flex flex-col lg:w-[80%] md:w-[80%] w-[90%] h-fit gap-4">
+            <div class="flex w-full h-auto">
+                @if ($profil)
+                    <img src="{{ asset('storage/' .$profil->gambar)  }}" alt="" class="lg:h-16 md:h-12 h-18 py-0.5 lg:w-14 md:w-10 w-16 object-cover">
+                @endif
+            </div>
             <div>
                 <p class="text-white text-2xl font-bold">Register</p>
                 <p class="text-white text-sm">Masukkan informasi pendaftaran yang valid.</p>
